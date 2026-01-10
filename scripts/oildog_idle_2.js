56ae9aa15f16e193a88b79669771b5e607072ddb
chlorinight <ppoppo26511@gmail.com>
2026-01-10 21:52:17 +0700

    main

--- scripts/oildog_idle_2.js
+++ scripts/oildog_idle_2.js
@@ -1,9 +1,41 @@
+/*
+
+		be aware, this piece of work will not be clean
+
+*/
+
 if (2 != 5) {
 		console.log("running")
 	} else {
 		console.log("WAHT THE FUCK")
 } // haha get the reference
 
-class dogs{
+// worth: how much money it sells for
+// accel: how fast it ages
+// income: passive income
+// lifespan: debuff, self-explanatory
+
+class dogHead{
+	constructor(worth,accel,income,lifespan){
+		this.worth = worth
+		this.accel = accel
+		this.income = income
+		this.lifespan = lifespan
+	}
+}
+
+
+class dog{
+	constructor(head,torso,frontlegs,hindlegs,tail,soul){
+		this.head = head
+		this.body = torso
+		this.frontLegs = frontlegs
+		this.backLegs = hindlegs
+		this.tail = tail
+		this.soul = soul
+		this.realage = 1
+		this.fakeage = 1
+	}
 
 }
+// head,torso,frontlegs,hindlegs,tail,soul,realage,fakeage,othervariables...
