fb60806f802b6d0bb9a05f01da19c8bb0ea38fbb
chlorinight <ppoppo26511@gmail.com>
2026-01-19 21:42:06 +0700

    tweaked dog age.

--- scripts/oildog_idle_2.js
+++ scripts/oildog_idle_2.js
@@ -35,8 +35,6 @@ function truncateTime(seconds){
 return [seconds,minutes,hours,days,years]
 };
 
-console.log(truncateTime(500000))
-
 let Money = new Decimal(0)
 
 let dFletters = [
@@ -48,24 +46,42 @@ let dMvowels = [
 ]
 
 let dLletters = [
-	'x','c','n','l','e','n','ll','p','b','d','s'
+	'x','c','n','l','e','n','ll','p','b','d','s','hn','t'
 ]
 
 let dLvowels = [
-	'ie','e','a','ouie','o','y','',''
+	'ie','e','a','ouie','o','y','ey','','','i','u','an','ay','','','a','e','ie','e','a','o','y','ey','','','i','u','an','ay','','','a','e'
 ]
 
-function dogName_new(){
-	let dfl = dFletters[Math.round(Math.random()*dFletters.length)]
-	let dmv = dMvowels[Math.round(Math.random()*dMvowels.length)]
-	let dll = dLletters[Math.round(Math.random()*dLletters.length)]
-	let dlv = dLvowels[Math.round(Math.random()*dLvowels.length)]
-
-	return (dfl+dmv+dll+dlv)
+function foundClassProperty_inArray(array,propertyname,propertyvalue){
+	let i
+	for (i=0;i<array.length;i++){
+		if (array[i][propertyname] == propertyvalue){
+			break
+		}
+	}
+	return array[i]
 }
 
-for (i=0;i<50;i++){
-	console.log(dogName_new)
+function dogName_new(){
+	let dfl = dFletters[Math.round(Math.random()*(dFletters.length-1))]
+	let dmv = dMvowels[Math.round(Math.random()*(dMvowels.length-1))]
+	let dll = dLletters[Math.round(Math.random()*(dLletters.length-1))]
+	let dlv = dLvowels[Math.round(Math.random()*(dLvowels.length-1))]
+	let royaltitle = ''
+	if (Math.round(Math.random()*100)==100){
+		let royalnumber = (Math.round(Math.random()*500)).toString() // fuck why is this shit so hard to code in
+		let numberending = 'th'
+		if (royalnumber[royalnumber.length - 1] == '1'){
+			numberending = 'st'
+		} else if (royalnumber[royalnumber.length - 1] == '2') {
+			numberending = 'nd'
+		} else if (royalnumber[royalnumber.length - 1] == '3') {
+			numberending = 'rd'
+		}
+		royaltitle = (' the '+royalnumber+numberending)
+	}
+	return (dfl+dmv+dll+dlv+royaltitle)
 }
 
 // load save
@@ -91,19 +107,20 @@ let dogStore = []
 // dog part classing bluh bluh
 
 // worth: how much money it sells for
-// accel: how fast it ages
+// accel: how fast it Ages
 // income: passive income
 // lifespan: self-explanatory, dies when the lifespan is reached if dog is mortal, and also used to calculate when the dog "dies" and turns into oil.
 // mortal: if the dog is mortal
 
 class dogHead{
-	constructor(sellworth,accel,income,lifespan,cost,mortal){
+	constructor(type,sellworth,accel,income,lifespan,cost,mortal){
 		this.worth = sellworth*sellMult
 		this.accel = accel
 		this.income = income
 		this.cost = cost
 		this.lifespan = lifespan
 		this.mortal = mortal
+		this.type = type
 	}
 } // basics
 
@@ -115,47 +132,62 @@ function checkifanyistrue(v){
 
 class dog{
 	constructor(head,torso,frontlegs,hindlegs,tail,soul){
-		this.identifier = (head+torso+frontlegs+hindlegs+tail+soul)
-		console.log("constructing dog");
+		let name = dogName_new()
+		if (foundClassProperty_inArray(dogStore,"name",name)){
+			let i = 0
+			let newName = (name+'_'+'1')
+			while (foundClassProperty_inArray(dogStore,"name",newName)){
+			i++
+			newName = (name+'_'+i.toString())
+			}
+			this.name = newName
+		} else {
+			this.name = name
+		}
 		this.head = head
 		this.body = torso
 		this.frontLegs = frontlegs
 		this.backLegs = hindlegs
 		this.tail = tail
 		this.soul = soul
-		this.realAge = new Decimal(1)
-		this.fakeAge = [0,0,0,0,0]
+		this.realAge = new Decimal(0)
+		this.Age = new Decimal(1)
+		this.displayAge = [0,0,0,0,0]
 		this.mortal = false
 		this.accel = 1
 		this.sellworth = 0
 	};
 	update(){
-		let realYear = this.realAge.divideBy(31536000)
+		let realYear = this.Age.divideBy(31536000)
 		let mantissa = realYear["mantissa"]
 		let exponent = realYear["exponent"]
-		if (isNaN(truncateTime(Math.floor(this.realAge))[0])) {
-			this.fakeAge = ((Math.round(mantissa*100)/100).toString() + "e+" + (Math.round(exponent*100)/100).toString() + " years")
+		if (isNaN(truncateTime(Math.floor(this.Age))[0])) {
+			this.displayAge = ((Math.round(mantissa*100)/100).toString() + "e+" + (Math.round(exponent*100)/100).toString() + " years")
 		} else {
-			let age = truncateTime(Math.floor(this.realAge))
-			let seconds = ((age[0].toString())+" seconds")
-			let minutes = ((age[1].toString())+" minutes")
-			let hours = ((age[2].toString())+" hours")
-			let days = ((age[3].toString())+" days")
-			let years = ((age[4].toString())+" years")
-			if (age[4]) {
-				this.fakeAge = (hours+" "+days+" "+years)
-			} else if (age[3]) {
-				this.fakeAge = (minutes+" "+hours+" "+days)
-			} else if (age[2]) {
-				this.fakeAge = (seconds+" "+minutes+" "+hours)
-			} else if (age[1]) {
-				this.fakeAge = (seconds+" "+minutes)
-			} else if (age[0]) {
-				this.fakeAge = (seconds)
+			let Age = truncateTime(Math.floor(this.Age))
+			let seconds = ((Age[0].toString())+" seconds")
+			let minutes = ((Age[1].toString())+" minutes")
+			let hours = ((Age[2].toString())+" hours")
+			let days = ((Age[3].toString())+" days")
+			let years = ((Age[4].toString())+" years")
+			if (Age[4]) {
+				this.displayAge = (hours+" "+days+" "+years)
+			} else if (Age[3]) {
+				this.displayAge = (minutes+" "+hours+" "+days)
+			} else if (Age[2]) {
+				this.displayAge = (seconds+" "+minutes+" "+hours)
+			} else if (Age[1]) {
+				this.displayAge = (seconds+" "+minutes)
+			} else if (Age[0]) {
+				this.displayAge = (seconds)
 			}
 		}
-		// this.accel = this.realAge // << REALLY exponential (for testing)
-		this.realAge = this.realAge.plus(this.realAge.times(this.accel))
+		// this.accel = this.Age // << REALLY exponential (for testing)
+		this.realAge = this.realAge.plus(1)
+		this.Age = this.Age.plus(this.Age.times(this.accel))
+		console.log(this.realAge)
+		console.log(this.Age)
+		console.log(this.displayAge)
 	}
 	sell(){
 		Money = Money + this.sellworth
@@ -174,19 +206,24 @@ function craftDog(head,torso,frontlegs,hindlegs,tail,soul){
 	limbsStore.splice(limbsStore.indexOf(tail),1)
 }
 
-/* debug
+/* debug */
 function debug_oilHead(){
-let oilHead = new dogHead(5,1,0,10,2,false,);
+let oilHead = new dogHead("oildog",5,1,0,10,2,false,);
 limbsStore = limbsStore.concat(oilHead)
-console.log(limbsStore)
 };
 function debug_dog(){
 craftDog(limbsStore[0],limbsStore[1],limbsStore[2],limbsStore[3],limbsStore[4],limbsStore[5])
-console.log(limbsStore)
-console.log(dogStore[0])
-console.log(dogStore)
 };
-*/
+function debug_fullset(){
+ debug_oilHead()
+  debug_oilHead()
+   debug_oilHead()
+    debug_oilHead()
+	 debug_oilHead()
+
+debug_dog()
+}
+/* */ 
 
 let frameCount = 0;
 
